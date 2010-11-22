48eb5b98f20e3f68ca4da8f5c38b1a52b70e70c0
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-11-22 21:20:58 -0200

    Detalhes para buscar servidores.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/OpenData/BR/Federal/PortalTransparencia/Servidores.pm
+++ lib/OpenData/BR/Federal/PortalTransparencia/Servidores.pm
@@ -3,18 +3,29 @@ package OpenData::BR::Federal::PortalTransparencia::Servidores;
 
 use Moose::Role;
 use HTML::TreeBuilder::XPath;
+use OpenData::Array;
 
-my $baseurl = 'http://www.portaltransparencia.gov.br/servidores';
-my $mainurl = join('/', $baseurl, 'Servidor-ListaServidores.asp');
+has baseurl => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'http://www.portaltransparencia.gov.br/servidores'
+);
+
+has mainurl => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub { join('/', shift->baseurl, 'Servidor-ListaServidores.asp') }
+);
 
 sub _servidores_parse_member {
     my ($self, $url) = @_;
-    my $people_url = join('/', $baseurl, $url);
+    my $people_url = join('/', $self->baseurl, $url);
     my $content = $self->get($people_url);
 
     my $tree = HTML::TreeBuilder::XPath->new_from_content($content);
    
-    warn 1;
+    warn $content;
 
     return (
         matricula       =>      '123456',
@@ -44,26 +55,26 @@ sub servidores_init {
     my $self = shift;
     my @servidores;
 
-    my $content = $self->get($mainurl);
-
+    my $content = $self->get($self->mainurl);
     my $tree = HTML::TreeBuilder::XPath->new_from_content($content);
-   
     my $root = $tree->findnodes("//table");
     my $table = $root->[0]->as_HTML;
+    my $items = OpenData::Array->new;
 
     $tree = HTML::TreeBuilder::XPath->new_from_content($table);
     $root = $tree->findnodes("//tr");
     
-    $self->_servidores_parse_tree($_) for (@{$root});
-
+    $items->add($self->_servidores_parse_tree($_)) for (@{$root});
+    
     $tree->delete;
+    
+    return $items->all;
 }
 
 sub run_servidores {
     my $self = shift;
-    $self->servidores_init;
+    return $self->servidores_init;
 }
-
 
 1;
 
