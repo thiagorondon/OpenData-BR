e0adc2cccae10f00886171ee792d6a0040803bbb
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2010-11-23 01:44:56 -0200

    adicionado paginacao

--- lib/OpenData/BR/Federal/PortalTransparencia/Servidores.pm
+++ lib/OpenData/BR/Federal/PortalTransparencia/Servidores.pm
@@ -4,6 +4,7 @@ package OpenData::BR::Federal::PortalTransparencia::Servidores;
 use Moose::Role;
 use HTML::TreeBuilder::XPath;
 use OpenData::Array;
+use URI;
 
 has baseurl => (
     is      => 'ro',
@@ -12,35 +13,36 @@ has baseurl => (
 );
 
 has mainurl => (
-    is      => 'ro',
-    isa     => 'Str',
-    lazy    => 1,
-    default => sub { join('/', shift->baseurl, 'Servidor-ListaServidores.asp') }
+    is   => 'ro',
+    isa  => 'Str',
+    lazy => 1,
+    default =>
+      sub { join( '/', shift->baseurl, 'Servidor-ListaServidores.asp' ) }
 );
 
 sub _servidores_parse_member {
-    my ($self, $url) = @_;
-    
+    my ( $self, $url ) = @_;
+
     return if $url =~ /Pagina/;
     my $id = $url;
     $id =~ s/^.*IdServidor=//;
-    
-    my $people_url = join('/', $self->baseurl, $url);
+
+    my $people_url = join( '/', $self->baseurl, $url );
     my $content = $self->get($people_url);
 
     my $tree = HTML::TreeBuilder::XPath->new_from_content($content);
     my $root = $tree->findnodes("//tr");
-    
+
     my $data = {};
     $data->{id} = $id;
 
-    foreach my $item (@{$root}) {
+    foreach my $item ( @{$root} ) {
         my $line = $item->as_text;
         $line =~ s/ *//;
-        my ($name, $value) = split(/:/, $line);
+        my ( $name, $value ) = split( /:/, $line );
         $value =~ s/ *// if $value;
         $data->{nome} = $value if $name eq 'Nome';
-        $data->{cpf} = $value if $name eq 'CPF';
+        $data->{cpf}  = $value if $name eq 'CPF';
     }
 
     return $data ? $data : undef;
@@ -48,36 +50,66 @@ sub _servidores_parse_member {
 }
 
 sub _servidores_parse_tree () {
-    my ($self, $html) = @_;
+    my ( $self, $html ) = @_;
 
     my $link = $html->as_HTML;
     $link =~ s/^.*href=\"//;
     $link =~ s/\".*$//;
     return $self->_servidores_parse_member($link);
-} 
-
+}
 
 sub _servidores_init {
     my $self = shift;
     my @servidores;
 
-    my $content = $self->get($self->mainurl);
-    my $tree = HTML::TreeBuilder::XPath->new_from_content($content);
-    my $root = $tree->findnodes("//table");
-    my $table = $root->[0]->as_HTML;
-    my $items = OpenData::Array->new;
+    my $content = $self->get( $self->mainurl );
 
-    $tree = HTML::TreeBuilder::XPath->new_from_content($table);
-    $root = $tree->findnodes("//tr");
-    
-    foreach my $obj (@{$root}) {
-        my $member = $self->_servidores_parse_tree($obj);
-        $items->add($member) if defined($member->{nome});
+    my $items      = OpenData::Array->new;
+    my $total_page = $self->_total_page($content);
+
+    for my $i ( 1 .. $self->_total_page($content) ) {
+
+        $self->debug("Paginacao, $i");
+        $content = $self->get( $self->_page( $self->mainurl, $i ) );
+        my $tree  = HTML::TreeBuilder::XPath->new_from_content($content);
+        my $root  = $tree->findnodes("//table");
+        my $table = $root->[0]->as_HTML;
+
+        $tree = HTML::TreeBuilder::XPath->new_from_content($table);
+        $root = $tree->findnodes("//tr");
+
+        foreach my $obj ( @{$root} ) {
+            my $member = $self->_servidores_parse_tree($obj);
+            $items->add($member) if defined( $member->{nome} );
+        }
+
+        $tree->delete;
+
     }
-    
-    $tree->delete;
-    
+
     return $items;
+}
+
+sub _page {
+    my ( $self, $url, $numero ) = @_;
+    my $u = URI->new($url);
+    $u->query_form( Pagina => $numero );
+    return $u->as_string;
+}
+
+sub _total_page {
+    my ( $self, $html ) = @_;
+    my $numero =
+      HTML::TreeBuilder::XPath->new_from_content($html)
+      ->findnodes('//p[@class="paginaAtual"]')->[0];
+    if ($numero) {
+        my $total = $numero->as_text;
+        $numero->delete;
+        return $1 if $total =~ /\d\/(\d+)/;
+    }
+    else {
+        die "Não conseguiu fazer a paginação";
+    }
 }
 
 sub run_servidores { shift->_servidores_init; }
