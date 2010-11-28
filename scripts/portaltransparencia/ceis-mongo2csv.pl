a09de762506d7b338cdc61af67b1757eda27f419
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-11-28 02:11:50 -0200

    Script para converter collection/mongodb ceis para csv.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ scripts/portaltransparencia/ceis-mongo2csv.pl
@@ -0,0 +1,23 @@
+
+use MongoDB;
+
+my $conn = MongoDB::Connection->new();
+
+my $db = $conn->opendata;
+my $coll = $db->ceis;
+my $cursor = $coll->query;
+
+my @fields = qw/nome fonte_data fonte cpfcnpj uf tipo data_inicial data_final orgao_sancionador/;
+
+print join(';', 'created', @fields, "\n");
+
+while ($cursor->has_next) {
+    my $data = $cursor->next;
+    
+    my @item;
+    push(@item, $data->{created});
+    push(@item, $data->{row}{$_}) for @fields;
+
+    print join(';', @item, "\n");
+}
+
