6dadce41b75b1418815c6fc238e2c9d223645cf1
Alexei Znamensky <russoz@cpan.org>
2010-11-30 20:00:17 -0200

    adicionados extractor, transformer e loader

---
+++ lib/OpenData/Extractor.pm
@@ -0,0 +1,17 @@
+
+package OpenData::Extractor;
+
+use Moose::Role;
+
+has id => ( is => 'ro', isa => 'Str', required => 1, );
+has name => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub { ucfirst( shift->id ) },
+);
+has description => ( is => 'ro', isa => 'Str', );
+
+requires 'extract';
+
+1;
