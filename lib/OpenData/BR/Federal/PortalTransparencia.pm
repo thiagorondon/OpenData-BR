d7f9fbc205a5e064db5b24da5183501af94eef37
Alexei Znamensky <russoz@cpan.org>
2010-11-30 17:33:42 -0200

    adicionado convenios, removidas subs comentadas

--- lib/OpenData/BR/Federal/PortalTransparencia.pm
+++ lib/OpenData/BR/Federal/PortalTransparencia.pm
@@ -10,8 +10,7 @@ with 'OpenData::Provider';
 has '+id'   => ( default => 'PortalTransparencia', );
 has '+name' => ( default => 'PortalTransparencia', );
 
-has '+description' =>
-  ( default => 'Dados fornecidos pelo Portal Transparencia: '
+has '+description' => ( default => 'Portal da Transparência: '
       . 'http://www.portaltransparencia.gov.br', );
 
 use OpenData::BR::Federal::PortalTransparencia::CEIS;
@@ -21,26 +20,14 @@ sub add_collection_ceis {
     shift->add_collection($c);
 }
 
+use OpenData::BR::Federal::PortalTransparencia::Convenios;
+
+sub add_collection_convenios {
+    my $c = OpenData::BR::Federal::PortalTransparencia::Convenios->new;
+    shift->add_collection($c);
+}
+
 #use OpenData::BR::Federal::PortalTransparencia::Servidores;
-#use OpenData::BR::Federal::PortalTransparencia::Convenios;
-
-#has dept => (
-#is      => 'rw',
-#isa     => 'Str',
-#default => 1
-#);
-
-#has current_collection => (
-#is        => 'rw',
-#isa       => 'Str',
-#predicate => 'has_current_collection',
-#clearer   => 'clear_current_collection',
-#trigger   => sub {
-#my ( $self, $new, $old ) = @_;
-#
-#$self->items->collection($new);
-#},
-#);
 
 has items => (
     is      => 'ro',
@@ -65,29 +52,6 @@ sub BUILD {
 
     #    $self->set_browser('Mechanize');
 }
-
-#sub _run {
-#    my $self = shift;
-#
-#    $self->confess('Must set a current_collection first!')
-#      unless $self->has_current_collection;
-#
-#    my $meth_name = '_run_' . $self->current_collection;
-#    $self->confess( 'There is no method ' . $meth_name )
-#      unless $self->can($meth_name);
-#    return eval '$self->' . $meth_name;
-#}
-
-#sub process {
-#    my $self = shift;
-#
-#    #$self->items->collection('servidores');
-#    #$self->items->collection('ceis');
-#    #$self->items->collection('convenios');
-#
-#    $self->current_collection('convenios');
-#    return $self->_run;
-#}
 
 1;
 
