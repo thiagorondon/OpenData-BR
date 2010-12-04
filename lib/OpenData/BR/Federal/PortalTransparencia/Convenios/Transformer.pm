cc96e1c400fe8a0c128edff9af1d526c71565a99
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-12-04 03:12:22 -0200

    Acertando Convenios.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ lib/OpenData/BR/Federal/PortalTransparencia/Convenios/Transformer.pm
@@ -0,0 +1,22 @@
+
+package OpenData::BR::Federal::PortalTransparencia::Convenios::Transformer;
+
+use Moose;
+use aliased 'OpenData::BR::Federal::PortalTransparencia::Convenios::Item';
+
+extends 'OpenData::Transformer::HTML';
+
+has '+node_xpath' =>
+  ( default => '//div[@id="listagemConvenios"]/table/tbody/tr', );
+
+has '+value_xpath' => ( default => '//td', );
+
+sub _transform_element {
+    my $self = shift;
+    my $data = shift;
+    my $item = Item->new_from_array($data);
+    return $item;
+}
+
+1;
+
