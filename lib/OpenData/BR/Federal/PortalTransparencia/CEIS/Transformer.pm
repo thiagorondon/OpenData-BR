11ef32eef1950fbe8a6752e150d43d6ae99681df
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-12-04 03:06:51 -0200

    Arquivos renomeados.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ lib/OpenData/BR/Federal/PortalTransparencia/CEIS/Transformer.pm
@@ -0,0 +1,23 @@
+
+package OpenData::BR::Federal::PortalTransparencia::CEIS::Transformer;
+
+use Moose;
+extends 'OpenData::Transformer::HTML';
+
+use aliased 'OpenData::BR::Federal::PortalTransparencia::CEIS::Item';
+
+has '+node_xpath' =>
+  ( default => '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr', );
+
+has '+value_xpath' => ( default => '//td', );
+
+sub _transform_element {
+    my $self = shift;
+    my $data = shift;
+
+    my $item = Item->new_from_array($data);
+    return $item;
+}
+
+1;
+
