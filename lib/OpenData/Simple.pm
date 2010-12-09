0cfd5b54e9951b796672e5901bc0f16e467d8505
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-12-09 00:28:28 -0200

    Adicionando uma versao simples do Simple.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ lib/OpenData/Simple.pm
@@ -0,0 +1,74 @@
+
+package OpenData::Simple;
+
+our $VERSION = '0.001';
+
+use Moose;
+use Devel::Declare ();
+use Carp qw/ confess /;
+use namespace::autoclean;
+use OpenData::Provider;
+
+extends 'Devel::Declare::Context::Simple';
+
+our $provider = undef;
+our $default_loader = undef;
+our $default_extract = undef;
+
+sub provider {
+    my ($hash) = shift;
+
+    die 'Provider need to be clean by clean_provider' if $provider;
+
+    my $collections = $hash->{collections};
+    delete $hash->{collections};
+
+    $default_loader = $hash->{default_loader};
+    delete $hash->{default_loader};
+
+    $default_extract = $hash->{default_extract};
+    delete $hash->{default_extract};
+
+    $provider = OpenData::Provider->new($hash);
+
+
+}
+
+sub clean_provider {
+    $provider = undef;
+    $default_loader = undef;
+    $default_extract = undef;
+}
+
+sub import {
+    my $class = shift;
+    my $caller = caller;
+    my $context = __PACKAGE__->new;
+    
+    my @cmds = ( 'provider' );
+
+    Devel::Declare->setup_for(
+        $caller, {
+            map { $_ => { const => sub { $context->parser(@_) } } } @cmds
+        }
+    );
+
+    no strict 'refs';
+    map { *{ join('::', $caller, $_) } = \&$_ } @cmds;
+}
+
+sub parser {
+    my $self = shift;
+
+    $self->init(@_);
+    $self->skip_declarator;
+
+    my $name = $self->strip_name;
+    my $proto = $self->strip_proto;
+
+}
+
+1;
+
+
+
