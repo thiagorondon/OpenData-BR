f04bbb993f030948e068fd871c627fe7add122ec
Alexei Znamensky <russoz@cpan.org>
2010-12-15 12:31:39 -0200

    exemplo implementado

--- scripts/wf-portaltransparencia.pl
+++ scripts/wf-portaltransparencia.pl
@@ -11,24 +11,44 @@ use aliased 'OpenData::AZ::ChainBox';
 use aliased 'OpenData::AZ::Box::LiteralData';
 use aliased 'OpenData::AZ::Box::HTMLFilter';
 use aliased 'OpenData::AZ::Box::URLRetriever';
-#use aliased 'OpenData::AZ::Box::URLRetrieverMultiPage';
+use aliased 'OpenData::AZ::Box::MultiPageURLGenerator';
+
+use URI;
+#use HTML::TreeBuilder::XPath;
 
 my $bigbox = ChainBox->new(
     chain => [
         LiteralData->new(
             data =>
-q{http://www.portaltransparencia.gov.br/ceis/EmpresasSancionadas.asp?paramEmpresa=0/}
+q{http://www.portaltransparencia.gov.br/ceis/EmpresasSancionadas.asp?paramEmpresa=0}
         ),
-        URLRetriever->new,
+        MultiPageURLGenerator->new(
+            first_page    => 34,
+            last_page     => 35,
+            make_page_url => sub {
+                my ( $self, $url, $page ) = @_;
+                my $u = URI->new($url);
+                $u->query_form( $u->query_form, Pagina => $page );
+                return $u->as_string;
+            },
+#            produce_last_page => sub{
+#                my ($self, $url) = @_;
+#
+#            },
+        ),
+        URLRetriever->new( deref => 1 ),
         HTMLFilter->new(
-            search_xpath => '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr'
+            search_xpath =>
+              '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr',
+            deref => 1,
         ),
-        HTMLFilter->new( search_xpath => '//td', deref => 1, result_type => 'VALUE'  ),
+#        HTMLFilter->new(
+#            search_xpath => '//td',
+#            deref        => 1,
+#            result_type  => 'VALUE',
+#        ),
     ],
 );
 
-local $, = "\n";
 use Data::Dumper;
-print Dumper($bigbox->output);
-
-
+print Dumper( $bigbox->output );
