0bdf5b2c46fbb262c7f88ade6c68576304867558
Alexei Znamensky <russoz@cpan.org>
2011-02-06 14:12:31 -0200

    removed the default value for table, and ran perltidy
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/OpenData/Flow/Node/SQL.pm
+++ lib/OpenData/Flow/Node/SQL.pm
@@ -9,21 +9,21 @@ use SQL::Abstract;
 my $sql = SQL::Abstract->new;
 
 has 'table' => (
-    is         => 'ro',
-    isa        => 'Str',
-    default    => 'table',
+    is       => 'ro',
+    isa      => 'Str',
     required => 1
 );
 
 has '+process_item' => (
     default => sub {
         return sub {
-            my ($self, $data) = @_;
-            my ($insert, @bind) = $sql->insert($self->table, $data);
+            my ( $self, $data ) = @_;
+            my ( $insert, @bind ) = $sql->insert( $self->table, $data );
+
             # TODO: regex ?
             map { $insert =~ s/\?/'$_'/; } @bind;
             print $insert . "\n";
-            }
+          }
     }
 );
 
