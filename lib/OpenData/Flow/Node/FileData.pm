567587c4c4cba3cb6c536c4d4ce105cd71739473
Alexei Znamensky <russoz@cpan.org>
2011-02-08 14:59:08 -0200

    added slurp option to FileData
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/OpenData/Flow/Node/FileData.pm
+++ lib/OpenData/Flow/Node/FileData.pm
@@ -20,6 +20,52 @@ has nochomp => (
     default => 0,
 );
 
+has do_slurp => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+has _get_item => (
+    is       => 'ro',
+    isa      => 'CodeRef',
+    required => 1,
+    lazy     => 1,
+    default  => sub {
+        my $self = shift;
+        #use Data::Dumper; print STDERR Dumper($self);
+
+        return sub {
+            #use Data::Dumper; print STDERR 'slurpy ' .Dumper($self);
+            my $fh   = $self->_handle;
+            my @slurp = <$fh>;
+            chomp @slurp unless $self->nochomp;
+            #use Data::Dumper; print STDERR 'slurpy ' .Dumper([@slurp]);
+            return [ @slurp ];
+          }
+          if $self->do_slurp;
+
+        # not a slurp, rather line by line
+        if ( $self->nochomp ) {
+            return sub {
+                #use Data::Dumper; print STDERR 'nochompy ' .Dumper($self);
+                my $fh = $self->_handle;
+                my $item = <$fh>;
+                return $item;
+            };
+        }
+        else {
+            return sub {
+                #use Data::Dumper; print STDERR 'chompy ' .Dumper($self);
+                my $fh = $self->_handle;
+                my $item = <$fh>;
+                chomp $item;
+                return $item;
+            };
+        }
+    },
+);
+
 sub _check_eof {
     my $self = shift;
     if ( $self->_handle->eof ) {
@@ -42,15 +88,14 @@ override 'process_input' => sub {
         $self->_check_eof;
     }
 
-    my $fh   = $self->_handle;
-    my $item = <$fh>;
-    chomp $item unless $self->nochomp;
+    my @item = ( $self->_get_item->() );
+    #use Data::Dumper; print STDERR 'items '.Dumper( [ @item ] );
 
     # check for EOF
     $self->_check_eof;
 
     # TODO some device to add multiple items (<infinity) to the output queue
-    $self->_add_output($item);
+    $self->_add_output( $self->_handle_list(@item));
 };
 
 1;
--- t/20-flow/21-filedata.t
+++ t/20-flow/21-filedata.t
@@ -1,4 +1,4 @@
-use Test::More tests => 10;
+use Test::More tests => 17;
 
 use_ok('OpenData::Flow::Node::FileData');
 new_ok('OpenData::Flow::Node::FileData');
@@ -19,9 +19,17 @@ ok( $data->output eq 'linha 6' );
 ok( $data->output eq 'linha 7' );
 
 ok( ! defined( $data->output ) );
-#ok( !( $data->output ) );
 
-#eval { $data->input('more input') };
-#ok( !$@ );
-#ok( !( $data->output ) );
-
+my $data2 = OpenData::Flow::Node::FileData->new(
+  initial_data => [ './examples/filedata.test' ],
+  do_slurp => 1,
+);
+my $res2 = $data2->output;
+#use Data::Dumper; print STDERR 'slurpy '.Dumper($res2);
+ok( $res2->[0] eq 'linha 1' );
+ok( $res2->[1] eq 'linha 2' );
+ok( $res2->[2] eq 'linha 3' );
+ok( $res2->[3] eq 'linha 4' );
+ok( $res2->[4] eq 'linha 5' );
+ok( $res2->[5] eq 'linha 6' );
+ok( $res2->[6] eq 'linha 7' );
