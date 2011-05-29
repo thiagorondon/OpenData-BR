67bb955774daa1a0c7632a4df0000b0e26fc4a57
Alexei Znamensky <russoz@cpan.org>
2011-05-28 21:19:12 -0300

    usando EGov::BR::Federal::PortalTransparencia::Flows::CEIS
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/EGov/BR/Federal/PortalTransparencia/Flows/CEIS.pm
+++ lib/EGov/BR/Federal/PortalTransparencia/Flows/CEIS.pm
@@ -10,13 +10,15 @@ use DataFlow 1.111450;
 use Params::Validate 0.98 qw(:all);
 
 sub new {
+    my $self = shift;
     my %opts = validate(
         @_,
         {
-            first_page     => { type => SCALAR, regex => qr/^\d+$/, },
-            last_page      => { type => SCALAR, regex => qr/^\d+$/, },
-            converter      => { type => SCALAR },
-            converter_opts => { type => HASHREF },
+            first_page =>
+              { type => SCALAR, regex => qr/^\-?\d+$/, optional => 1 },
+            last_page => { type => SCALAR, regex => qr/^\d+$/, optional => 1 },
+            converter      => { type => SCALAR,  optional => 1 },
+            converter_opts => { type => HASHREF, optional => 1 },
         },
     );
 
@@ -44,14 +46,12 @@ sub new {
         },
         Encoding => { from => 'iso-8859-1', to => 'utf8' },
     ];
-    $procs->push( $opts{converter} ) if exists $opts{converter};
-	die q{Parameter 'converter_opts' requires the parameter 'converter'} if
-	  ( !exists $opts{converter} && $opts{converter_opts} );
-    $procs->push( $opts{converter_opts} ) if exists $opts{converter_opts};
+    push( @{$procs}, $opts{converter} ) if exists $opts{converter};
+    die q{Parameter 'converter_opts' requires the parameter 'converter'}
+      if ( !exists $opts{converter} && $opts{converter_opts} );
+    push( @{$procs}, $opts{converter_opts} ) if exists $opts{converter_opts};
 
     my $flow = DataFlow->new($procs);
-
-##############################################################################
 
     my $base = q{http://www.portaltransparencia.gov.br} . '/'
       . q{ceis/EmpresasSancionadas.asp?paramEmpresa=0};
--- scripts/portaltransparencia/ceis.pl
+++ scripts/portaltransparencia/ceis.pl
@@ -5,56 +5,38 @@ use warnings;
 use FindBin qw($Bin);
 use lib "$Bin/../../lib";
 
-use DataFlow 1.111450;
 use Data::Dumper;
 
-my $flow = DataFlow->new(
-    [
-        'EGov::BR::Federal::PortalTransparencia::Util::Paginar' =>
-          { first_page => -1, deref => 1, },
-        'URLRetriever',
-        HTMLFilter => {
-            search_xpath =>
-              '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr',
-        },
-        HTMLFilter => {
-            search_xpath => '//td',
-            result_type  => 'VALUE',
-            ref_result   => 1,
-        },
-        sub {    # remove leading and trailing spaces
-            local $_ = shift;
-            s/^\s*//;
-            s/\s*$//;
-            return $_;
-        },
-        Encoding => { from => 'iso-8859-1', to          => 'utf8' },
-        NOP      => { name => 'espiando',   dump_output => 1, },
-        CSV      => {
-            name          => 'csv',
-            direction     => 'TO_CSV',
-            text_csv_opts => { binary => 1 },
-            headers       => [
-                'CNPJ/CPF',   'Nome/Razão Social/Nome Fantasia',
-                'Tipo',       'Data Inicial',
-                'Data Final', 'Nome do Órgão/Entidade',
-                'UF',         'Fonte',
-                'Data'
-            ],
-            dump_output => 1,
-        },
-        SimpleFileOutput => { file => '> /tmp/ceis.csv', ors => "\n" },
-    ],
+use EGov::BR::Federal::PortalTransparencia::Flows::CEIS;
+
+my $flow = EGov::BR::Federal::PortalTransparencia::Flows::CEIS->new(
+
+# PARA RESTRINGIR PAGINAS A SEREM SCRAPE-ADAS, USE OS PARÂMETROS:
+    first_page => -1,
+
+#    last_page => 50,
+
+# PARA HABILITAR CONVERSÃO PARA FORMATO CSV, DESCOMENTE LINHAS ABAIXO.
+# (Similarmente para JSON, YAML ou MessagePack)
+#
+#    converter      => 'CSV',
+#    converter_opts => {
+#        name          => 'csv',
+#        direction     => 'CONVERT_TO',
+#        text_csv_opts => { binary => 1, sep_char => ';', },
+#        headers       => [
+#            'CNPJ/CPF',   'Nome/Razão Social/Nome Fantasia',
+#            'Tipo',       'Data Inicial',
+#            'Data Final', 'Nome do Órgão/Entidade',
+#            'UF',         'Fonte',
+#            'Data'
+#        ],
+#        dump_output => 1,
+#    },
+
 );
-
-##############################################################################
-
-my $base = q{http://www.portaltransparencia.gov.br} . '/'
-  . q{ceis/EmpresasSancionadas.asp?paramEmpresa=0};
-
-$flow->input($base);
 
 my @res = $flow->flush;
 
-#print Dumper(\@res);
+print Dumper( \@res );
 
