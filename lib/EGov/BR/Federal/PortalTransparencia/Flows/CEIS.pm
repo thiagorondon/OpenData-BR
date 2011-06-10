306d000c529fdc9875a31a6c238bd68ace3e2b10
Alexei Znamensky <russoz@cpan.org>
2011-06-10 09:00:57 -0300

    CEIS.pm é um módulo Moose de verdade agora
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/EGov/BR/Federal/PortalTransparencia/Flows/CEIS.pm
+++ lib/EGov/BR/Federal/PortalTransparencia/Flows/CEIS.pm
@@ -3,63 +3,76 @@ package EGov::BR::Federal::PortalTransparencia::Flows::CEIS;
 use strict;
 use warnings;
 
+use Moose;
+extends 'DataFlow' => { -version => 1.111600 };
+
 use namespace::autoclean;
 
-use autobox;
-use DataFlow 1.111450;
-use Params::Validate 0.98 qw(:all);
+use EGov::BR::Federal::PortalTransparencia::Util::Paginar;
 
-sub new {
-    my $self = shift;
-    my %opts = validate(
-        @_,
-        {
-            first_page =>
-              { type => SCALAR, regex => qr/^\-?\d+$/, optional => 1 },
-            last_page => { type => SCALAR, regex => qr/^\d+$/, optional => 1 },
-            converter      => { type => SCALAR,  optional => 1 },
-            converter_opts => { type => HASHREF, optional => 1 },
-        },
-    );
+has 'BASE_URL' => (
+    is      => 'ro',
+    default => q{http://www.portaltransparencia.gov.br} . '/'
+      . q{ceis/EmpresasSancionadas.asp?paramEmpresa=0},
+    init_arg => undef
+);
 
-    my $opt_pg = { deref => 1 };
-    $opt_pg->{first_page} = $opts{first_page} if exists $opts{first_page};
-    $opt_pg->{last_page}  = $opts{last_page}  if exists $opts{last_page};
+has 'first_page' => (
+    'is'  => 'ro',
+    'isa' => 'Int',
+);
 
-    my $procs = [
-        'EGov::BR::Federal::PortalTransparencia::Util::Paginar' => $opt_pg,
-        'URLRetriever',
-        HTMLFilter => {
-            search_xpath =>
-              '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr',
-        },
-        HTMLFilter => {
-            search_xpath => '//td',
-            result_type  => 'VALUE',
-            ref_result   => 1,
-        },
-        sub {    # remove leading and trailing spaces
-            local $_ = shift;
-            s/^\s*//;
-            s/\s*$//;
-            return $_;
-        },
-        Encoding => { from => 'iso-8859-1', to => 'utf8' },
-    ];
-    push( @{$procs}, $opts{converter} ) if exists $opts{converter};
-    die q{Parameter 'converter_opts' requires the parameter 'converter'}
-      if ( !exists $opts{converter} && $opts{converter_opts} );
-    push( @{$procs}, $opts{converter_opts} ) if exists $opts{converter_opts};
+has 'last_page' => (
+    'is'  => 'ro',
+    'isa' => 'Int',
+);
 
-    my $flow = DataFlow->new($procs);
+has '_pager' => (
+    is      => 'ro',
+    isa     => 'EGov::BR::Federal::PortalTransparencia::Util::Paginar',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
 
-    my $base = q{http://www.portaltransparencia.gov.br} . '/'
-      . q{ceis/EmpresasSancionadas.asp?paramEmpresa=0};
+        my $opt_pg = { deref => 1 };
+        $opt_pg->{first_page} = $self->first_page if $self->first_page;
+        $opt_pg->{last_page}  = $self->last_page  if $self->last_page;
 
-    $flow->input($base);
+        return EGov::BR::Federal::PortalTransparencia::Util::Paginar->new(
+            $opt_pg);
+    },
+    init_arg => undef,
+);
 
-    return $flow;
-}
+has '+procs' => (
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return [
+            $self->_pager,
+            'URLRetriever',
+            [
+                HTMLFilter => {
+                    search_xpath =>
+                      '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr',
+                }
+            ],
+            [
+                HTMLFilter => {
+                    search_xpath => '//td',
+                    result_type  => 'VALUE',
+                    ref_result   => 1,
+                }
+            ],
+            sub {    # remove leading and trailing spaces
+                s/^\s*//;
+                s/\s*$//;
+                return $_;
+            },
+            [ Encoding => { from => 'iso-8859-1', to => 'utf8' } ],
+        ];
+    }
+);
 
 1;
 
--- scripts/portaltransparencia/ceis.pl
+++ scripts/portaltransparencia/ceis.pl
@@ -5,37 +5,39 @@ use warnings;
 use FindBin qw($Bin);
 use lib "$Bin/../../lib";
 
-use Data::Dumper;
+use Data::Printer colored => 0;
 
 use EGov::BR::Federal::PortalTransparencia::Flows::CEIS;
 
-my $flow = EGov::BR::Federal::PortalTransparencia::Flows::CEIS->new(
+my $ceis = EGov::BR::Federal::PortalTransparencia::Flows::CEIS->new(
 
 # PARA RESTRINGIR PAGINAS A SEREM SCRAPE-ADAS, USE OS PARÂMETROS:
 #    first_page => -1,
 #    last_page => 50,
-
-# PARA HABILITAR CONVERSÃO PARA FORMATO CSV, DESCOMENTE LINHAS ABAIXO.
-# (Similarmente para JSON, YAML ou MessagePack)
-#
-#    converter      => 'CSV',
-#    converter_opts => {
-#        name          => 'csv',
-#        direction     => 'CONVERT_TO',
-#        text_csv_opts => { binary => 1, sep_char => ';', },
-#        headers       => [
-#            'CNPJ/CPF',   'Nome/Razão Social/Nome Fantasia',
-#            'Tipo',       'Data Inicial',
-#            'Data Final', 'Nome do Órgão/Entidade',
-#            'UF',         'Fonte',
-#            'Data'
-#        ],
-#        dump_output => 1,
-#    },
-
 );
 
-my @res = $flow->flush;
+my $flow = DataFlow->new( [
+	$ceis,
+	[ CSV => {
+        name          => 'csv',
+        direction     => 'CONVERT_TO',
+        text_csv_opts => { binary => 1, sep_char => ';', },
+        headers       => [
+            'CNPJ/CPF',   'Nome/Razão Social/Nome Fantasia',
+            'Tipo',       'Data Inicial',
+            'Data Final', 'Nome do Órgão/Entidade',
+            'UF',         'Fonte',
+            'Data'
+        ],
+        dump_output => 1,
+     },
+	 ],
+	 ] );
 
-print Dumper( \@res );
+
+$flow->input($ceis->BASE_URL);
+my @res = $flow->flush;
+p @res;
+
+#print Dumper( \@res );
 
